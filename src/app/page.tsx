69a6512f14cdb8ae8dc148ff30c7d48d8fb67a79
Blake Hatch <blakewihatch@gmail.com>
2024-03-30 12:09:06 -0400

    Made primary route dynamic

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -6,6 +6,7 @@ import EthConnectWallet from '@/components/EthConnectWallet'
 import SolanaConnectWallet from '@/components/SolanaConnectWallet'
 import { getXataClient } from '../xata';
 
+export const dynamic = 'force-dynamic';
 
 export default async function Home() {
   const xata = getXataClient();
